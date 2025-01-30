c249006404e27fd0e1aceac6a2e8783d5da19f3f
rlkiran <rlkiran536@gmail.com>
2025-01-30 16:26:29 +0530

    cors added for local testing

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.item_routes import item_routes
 
 app = FastAPI()
+# Allow all origins (change this to specific origins if needed)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can replace "*" with ["http://127.0.0.1:5500"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
 app.include_router(item_routes)
